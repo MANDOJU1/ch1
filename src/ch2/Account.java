d9d40c1d4e359770543b449fb81d38c635819e42
minju <ngit0001@gmail.com>
2024-05-08 12:47:11 +0900

    getOwner 메서드 추가

--- src/ch2/Account.java
+++ src/ch2/Account.java
@@ -4,9 +4,12 @@ public class Account {
     private String owner;
     private int balance;
     private String accountNo;
-    
-    public String getAccountNo(){
+
+    public String getAccountNo() {
         return accountNo;
     }
 
+    public String getOwner(){
+        return owner;
+    } 
 }
