1210ac107d92faaa1bc7068f68c1bc39003c2236
minju <ngit0001@gmail.com>
2024-05-08 12:10:54 +0900

    for 문 추가

---
+++ src/ch1/NumberEx.java
@@ -0,0 +1,10 @@
+package ch1;
+
+public class NumberEx {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
